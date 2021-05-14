33d6e86c5a5b1416117254e086b3b54e339b1e8b
Doumas Georgios (Δούμας Γεώργιος) <geodoomgr@yahoo.gr>
2021-05-14 20:20:51 +0200

    Update InheritFromMoreClasses.py

--- InheritFromMoreClasses.py
+++ InheritFromMoreClasses.py
@@ -1,7 +1,3 @@
-# The following does not work, it seems that the way I tried to use
-# super(name-of-base-class).method-of-base-class
-# is not accepted syntax. So I did not want to spent time to try things out
-# and abandonded it
 
 class Animal:
     def __init__(self, animal_name, animal_size):
@@ -12,75 +8,78 @@ class Animal:
         return f"animal({self.size})"
 
 class Movements:
-    def walk(walking_speed):
-        print(f"walking in {speed} m/s")
+    def walk(self, walking_speed=None):
         self.speed = walking_speed
-    def swim(swimming_speed, swim_depth):
+        print(f"walking in {self.speed} m/s")
+    def swim(self, swimming_speed=None, swim_depth=None):
         self.speed = swimming_speed
         self.depth = swim_depth
-        print(f"swimming in {speed} m/s in depth of {depth} m")
-    def fly(height, flying_speed):
+        print(f"swimming in {self.speed} m/s in depth of {self.depth} m")
+    def fly(self, height, flying_speed):
         self.altitude = height
         self.velocity = flying_speed
-        print(f"flying at {altitude} m with {velocity} m/s")
-    def hop(jump):
+        print(f"flying at {self.altitude} m with {self.velocity} m/s")
+    def hop(self, jump):
         self.distance = jump
-        print(f" hopping {distance} m long")
+        print(f" hopping {self.distance} m long")
     
 class Wolves(Animal, Movements):
     def __init__(self, animal_name, animal_size, walking_speed):
         if animal_size in ["small", "medium"]:
-            super(Animal).__init__(animal_name, animal_size)
-            super(Movements).walk(walking_speed)
+            super().__init__(animal_name, animal_size)
+            super().walk(walking_speed)
     def __repr__(self):
         return f"Wolves({self.size}, {self.name})"
 
 class Bears(Animal, Movements):
     def __init__(self, animal_name, animal_size, walking_speed):
         if animal_size in [ "medium", "large"]:
-            super(Animal).__init__(animal_name, animal_size)
-            super(Movements).walk(walking_speed)
+            super().__init__(animal_name, animal_size)
+            super().walk(walking_speed)
     def __repr__(self):
         return f"Bears({self.size}, {self.name})"
 
 class Eagles(Animal, Movements):
     def __init__(self, animal_name, animal_size, fl_height, fl_speed):
         if animal_size in ["small", "medium"]:
-            super(Animal).__init__(animal_name, animal_size)
-            super(Movements).fly(fl_height, fl_speed)
+            super().__init__(animal_name, animal_size)
+            super().fly(fl_height, fl_speed)
     def __repr__(self):
         return f"Eagles({self.size}, {self.name})"
 
 class Ducks(Animal, Movements):
     def __init__(self, animal_name, animal_size, sw_depth, sw_speed):
         if animal_size in ["small", "medium"]:
-            super(Animal).__init__(animal_name, animal_size)
-            super(Movements).swim(sw_speed, sw_depth)
+            super().__init__(animal_name, animal_size)
+            super().swim(sw_speed, sw_depth)
     def __repr__(self):
         return f"Ducks({self.size}, {self.name})"
 
 class Frogs(Animal, Movements):
     def __init__(self, animal_name, sw_depth, sw_speed, jump_length):
         super().__init__(animal_name, animal_size="small")
-        super(Movements).swim(sw_speed, sw_depth)
-        super(Movements).hop(jump_length)
+        super().swim(sw_speed, sw_depth)
+        super().hop(jump_length)
     def __repr__(self):
         return f"Frogs({self.name}, {self.depth}, {self.size})"
 
 class Trout(Animal, Movements):
     def __init__(self, animal_name, sw_depth, sw_speed):
         super().__init__(animal_name, animal_size="small")
-        super(Movements).swim(sw_speed, sw_depth)
+        super().swim(sw_speed, sw_depth)
     def __repr__(self):
         return f"Trout({self.name}, {self.depth}, {self.size})"
 
 wolf01 = Wolves("scoopy-doo", "small", 3)
 wolf02 = Wolves("greywolf", "medium", 5)
 print(wolf01)
+wolf01.walk()
 print(wolf02)
 bear01 = Bears("arthur", "medium", 4)
 print(bear01)
 frog01 = Frogs("king", 2, 5, 1)
 print(frog01)
-frog01.swim()
+# I do not want the frog to be able to walk,
+# and this is the problem with my desing (having 1 big Movement class with many methods)
+frog01.walk(5)  
 
